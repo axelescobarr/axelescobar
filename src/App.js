25686953ef6b9ae80333fa5604f4990ccf3de579
axelescobarr <axelescobarschneider@gmail.com>
2023-03-14 14:58:13 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+import logo from '../../imagenes/logo.png';
+import { useState } from 'react';
+import { Icon } from '@iconify/react';
+import { Link} from "react-scroll";
+
+
+function Navbar() {
+
+
+    const [state, setState] = useState(false);
+
+    const menuHandler = () => {
+        setState(!state)
+    }
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logoContainer}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+      </div>
+      <button className={styles.btn} onClick={menuHandler}><Icon icon="icon-park:hamburger-button" width="40" height="40"  /></button>
+      <ul className={state == false ? styles.navLinks : styles.navLinksMin}>
+      
+        <li><Link activeClass="active" to="Main_mainContainer__0MG5C" spy={true} smooth={true} duration={500}>Inicio</Link></li>
+        <li><Link activeClass="active" to="Main_aboutMe__9BEq8" spy={true} smooth={true} duration={500}>Sobre Mi</Link></li>
+        <li><Link activeClass="active" to="Main_projects__2HY7t" spy={true} smooth={true} duration={500}>Proyectos</Link></li>
+        <li><Link activeClass="active" to="Main_contacts__f6DNy" spy={true} smooth={true} duration={500}>Contacto</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
